2eac68734913e3060d91d9bd1d2edb6b432efef9
Jbbouille <beanbaptiste.petit@gmail.com>
2019-06-08 13:40:18 +0200

    Add creation command.

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,21 +1,37 @@
-CREATE TABLE secret (
-    digest TEXT NOT NULL,
+CREATE TABLE secret
+(
+    digest       TEXT    NOT NULL,
     access_right INTEGER NOT NULL
 );
 
-CREATE TABLE music_score (
-    title TEXT NOT NULL,
-    file_name TEXT NOT NULL,
-    style TEXT,
-    other_files BLOB,
-    lyrics TEXT,
-    data_path_id INTEGER NOT NULL ,
-    FOREIGN KEY(data_path_id) REFERENCES data_path(id)
+CREATE TABLE music_score
+(
+    title        TEXT    NOT NULL,
+    file_name    TEXT    NOT NULL,
+    style        TEXT,
+    other_files  BLOB,
+    lyrics_id    INTEGER NOT NULL,
+    data_path_id INTEGER NOT NULL,
+    FOREIGN KEY (lyrics_id) REFERENCES music_style (id),
+    FOREIGN KEY (data_path_id) REFERENCES data_path (id)
 );
 
-create table data_path (
+create table music_style
+(
+    id    INTEGER PRIMARY KEY,
+    style TEXT NOT NULL
+);
+
+create table data_path
+(
     id   INTEGER PRIMARY KEY,
     path TEXT NOT NULL
 );
 
-CREATE VIRTUAL TABLE music_index USING fts5(title, file_name, lyrics, style);
+CREATE VIRTUAL TABLE music_index USING fts5
+(
+    title,
+    file_name,
+    lyrics,
+    style
+);
--- src/create.rs
+++ src/create.rs
@@ -1,29 +1,32 @@
-use core::borrow::Borrow;
-use std::path::PathBuf;
+use std::ffi::{OsStr, OsString};
+use std::os::raw::c_uint;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use walkdir::{DirEntry, WalkDir};
+use std::fs::create_dir_all;
 
 enum MusicStyle {
-    Agnus,
-    Alleluia,
-    Acclamations,
-    Common,
-    Gloria,
-    Kyrie,
-    Psaumes,
-    Sanctus,
-    Anamnese,
-    Taize,
-    Messes,
+    Agnus = 1,
+    Alleluia = 2,
+    Acclamations = 3,
+    Common = 4,
+    Gloria = 5,
+    Kyrie = 6,
+    Psaumes = 7,
+    Sanctus = 8,
+    Anamnese = 9,
+    Taize = 10,
+    Messes = 11,
 }
 
 struct DbEntry {
     title: String,
     file_name: String,
-    style: Vec<u8>,
-    other_files: Vec<String>,
+    style: Vec<i8>,
+    other_files: Option<Vec<String>>,
     lyrics: String,
-    data_path_id: i8,
+    data_path_id: u8,
 }
 
 pub fn create_database(input: PathBuf, output_db_name: PathBuf, data_path: PathBuf) {
@@ -65,11 +68,108 @@ fn create_db_entry(entry: Result<DirEntry, walkdir::Error>) -> Option<DbEntry> {
 fn create_from_pdf(file_entry: DirEntry) -> Option<DbEntry> {
     debug!("Found PDF entry {:?}.", file_entry);
 
+    let pdftotext = Command::new("pdftotext")
+        .arg("-enc")
+        .arg("UTF-8")
+        .arg(file_entry.path())
+        .arg("-")
+        .output()
+        .expect("failed to execute pdftotext process.");
+
+    let lyrics = String::from_utf8(pdftotext.stdout).expect("");
+
+    trace!("pdftotext command result {:?}.", lyrics);
+
+    let title_raw = file_entry.path()
+        .file_name()
+        .expect("Cannot convert file name")
+        .to_os_string();
+
+    let title = String::from(title_raw.to_str().expect("Cannot convert to &str"));
+    let style: Vec<i8> = find_music_style(&lyrics, &title, &title).into_iter().map(|music_style: MusicStyle| music_style as i8).collect();
+    let file_name = title.clone();
+
+    if !lyrics.trim().is_empty() {
+        return Some(
+            DbEntry {
+                title,
+                file_name,
+                style,
+                other_files: None,
+                lyrics,
+                data_path_id: 1,
+            })
+    }
+    let tmp_directory = String::from("/tmp/mtsundays/");
+    create_dir_all(&tmp_directory)
+        .expect("Creation of working tmp dir fails.");
+
+    trace!("Extracting image from {:?}", file_entry.path());
+    let file_name_str = title_raw.to_str().expect("Cannot convert path to string.");
+
+    let pdfimages = Command::new("pdfimages")
+        .arg("-j")
+        .arg(file_entry.path())
+        .arg(format!("{}{}", &tmp_directory, file_name_str))
+        .output()
+        .expect("Failed to execute pdfimages process.");
+
+    for entry in WalkDir::new(tmp_directory) {
+        let file_entry: DirEntry = entry.expect("Cannot get result entry.");
+        let image_extracted_name = file_entry.file_name().to_str().expect("Cannot convert file_name to str.");
+
+        if !image_extracted_name.contains(file_name_str) {
+            trace!("Image {} does not need to be converted.", image_extracted_name);
+            continue;
+        };
+
+        trace!("Found image {:?} to convert.", file_entry.path());
+        let tesseract = Command::new("tesseract")
+            .arg(file_entry.path())
+            .arg("stdout")
+            .output()
+            .expect("Failed to execute tesseract process.");
+
+        trace!("Tesseract command result {:?}.", lyrics);
+        let lyrics = String::from_utf8(tesseract.stdout);
+    };
+
     None
 }
 
 fn create_from_picture(file_entry: DirEntry) -> Option<DbEntry> {
     debug!("Found PICTURE entry {:?}.", file_entry);
 
-    None
+    let tesseract = Command::new("tesseract")
+        .arg(file_entry.path())
+        .arg("stdout")
+        .output()
+        .expect("Failed to execute tesseract process.");
+
+    let lyrics = String::from_utf8(tesseract.stdout);
+
+    trace!("Tesseract command result {:?}.", lyrics);
+    let title_raw = file_entry.path()
+        .file_name()
+        .expect("Cannot convert file name")
+        .to_os_string();
+
+    let title = String::from(title_raw.to_str().expect("Cannot convert to &str"));
+    let lyrics = lyrics.expect("Cannot find lyrics.");
+    let style: Vec<i8> = find_music_style(&lyrics, &title, &title).into_iter().map(|music_style: MusicStyle| music_style as i8).collect();
+    let file_name = title.clone();
+
+    Some(
+        DbEntry {
+            title,
+            file_name,
+            style,
+            other_files: None,
+            lyrics,
+            data_path_id: 1,
+        })
+}
+
+fn find_music_style(lyrics: &str, file_name: &str, title: &str) -> Vec<MusicStyle> {
+    vec![MusicStyle::Acclamations]
 }
